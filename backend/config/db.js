85867f46acd264366ac5cf91d4c0be8dbac27480
SikdarRudra <onsrudrasikdar@gmail.com>
2025-12-06 15:14:12 +0530

    Remove unnecessary options from mongoose connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      bufferCommands: false,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("db connected");
   } catch (error) {
     console.log("db error");

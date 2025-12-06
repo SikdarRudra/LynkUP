cee0a3afadb919a1ce4bba4b6cde855b2dd2e637
SikdarRudra <onsrudrasikdar@gmail.com>
2025-12-06 15:02:26 +0530

    Add testing response

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-const connectDb=async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log("db connected")
-    } catch (error) {
-         console.log("db error")
-    }
-}
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      bufferCommands: false,
+    });
+    console.log("db connected");
+  } catch (error) {
+    console.log("db error");
+  }
+};
 
-export default connectDb
+export default connectDb;
